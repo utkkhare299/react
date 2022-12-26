7e42643bd62eae66c1702d5f16dd53fcde127d4f
utkarsh khare <utkkhare999@gmail.com>
2022-12-27 00:23:41 +0530

    first commit2

---
+++ App.js
@@ -0,0 +1,11 @@
+import Expenseitem from "./components/expenseitem";
+
+function App() {
+  return (
+    <div>
+      <Expenseitem></Expenseitem>
+    </div>
+  );
+}
+
+export default App;
---
+++ components/expenseitem.js
@@ -0,0 +1,11 @@
+function Expenseitem() {
+  return (
+    <div>
+      <h2> Expense items </h2>
+      <div> Food Rs 10</div>
+      <div> Petrol Rs 100 </div>
+      <div> Movies Rs 200 </div>
+    </div>
+  );
+}
+export default Expenseitem;
