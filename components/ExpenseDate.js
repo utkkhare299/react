0d1f096816047b3fcaa3210c7ba63d684b3b0dc6
utkarsh khare <utkkhare999@gmail.com>
2023-01-01 10:53:53 +0530

    onclick

--- components/ExpenseDate.js
+++ components/ExpenseDate.js
@@ -1,5 +1,5 @@
 const ExpenseDate = ({ date }) => {
-    const month = date.toLocaleString('en-US', { month: 'long' })
+    const month = date.toLocaleString('en-US', { month: 'long' })           
     const year = date.getFullYear()
     const day = date.toLocaleString('en-US', { day: '2-digit' })
     return (
@@ -12,3 +12,8 @@ const ExpenseDate = ({ date }) => {
   }
   
   export default ExpenseDate
+
+  // props for passing data into through configure
+  //components 
+  //the approach of building a user interface from smaller buildingg blocks is called composition
+  // 
--- components/expenseitem.js
+++ components/expenseitem.js
@@ -2,11 +2,16 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
 const ExpenseItem = ({ amount, date, title, location }) => {
+  const handleClick = (e) => {
+    e.target.parentElement.style.display = "none";
+  };
+
   return (
     <div className="expense-item">
       <ExpenseDate date={date} />
       <h3> {location}</h3>
       <ExpenseDetails amount={amount} title={title} />
+      <button onClick={handleClick}>Delete Expense</button>
     </div>
   );
 };
