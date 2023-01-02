100e61d4780d6e47b905de27d2cc19785b225cd4
utkarsh khare <utkkhare999@gmail.com>
2023-01-02 11:53:29 +0530

    chnage expense

--- components/expenseitem.js
+++ components/expenseitem.js
@@ -1,17 +1,19 @@
+import { useState } from 'react'
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
 const ExpenseItem = ({ amount, date, title, location }) => {
+  const [newAmount, setNewAmount] = useState(amount)
   const handleClick = (e) => {
-    e.target.parentElement.style.display = "none";
+    setNewAmount(100)
   };
 
   return (
     <div className="expense-item">
       <ExpenseDate date={date} />
       <h3> {location}</h3>
-      <ExpenseDetails amount={amount} title={title} />
-      <button onClick={handleClick}>Delete Expense</button>
+      <ExpenseDetails amount={newAmount} title={title} />
+      <button onClick={handleClick}>Change Expense</button>
     </div>
   );
 };
