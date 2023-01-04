20af5b2a4d550edd45fffa4ea21e069d067c696c
utkarsh khare <utkkhare999@gmail.com>
2023-01-04 22:25:37 +0530

    10

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import "./Expenseitem.css";
+import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 import ExpenseForm from "./components/ExpenseForm";
 import { useState } from "react";
@@ -33,6 +34,29 @@ const expenses = [
   },
 ];
 export default function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   const [newExpenses, setNewExpenses] = useState(expenses);
 
   const saveExpenseDataHandler = (data) => {
@@ -47,14 +71,14 @@ export default function App() {
 
       <main>
         <h1>Expense Items</h1>
-        {newExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {
+        expenses.map(expense => (
+          <ExpenseItem key={expense.id} 
+          title= {expense.title} 
+          amount={expense.amount} 
+          date={expense.date} />
+        ))
+      }
       </main>
     </>
   );
--- components/expenseitem.js
+++ components/expenseitem.js
@@ -9,11 +9,15 @@ const ExpenseItem = ({ amount, date, title }) => {
   };
 
   return (
-    <div className="expense-item">
-      <ExpenseDate date={date} />
-      <ExpenseDetails amount={newAmount} title={title} />
-      <button onClick={handleClick}>Change Expense</button>
+  
+  
+      <div className="expense-item">
+      <div>{date.toISOString()}</div>
+      <div className="expense-item__description">
+         <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
+      </div>
     </div>
-  );
+  )
 };
 export default ExpenseItem;
